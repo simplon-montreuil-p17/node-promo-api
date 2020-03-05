2ed28cc6536d70a41d05fb2a3bb88d86d97846ef
Sandro Micalizzi <54776082+sandrocode@users.noreply.github.com>
2020-03-05 15:04:54 +0100

    query string

--- routes/promos.routes.js
+++ routes/promos.routes.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const promosController = require("../controllers/promos.controller");
-const validators = require('../validators/promos');
+const validators = require("../validators/promos");
 
 router.get("/", async (request, response) => {
   const data = await promosController.getAllPromo();
   response.status(200).json(data);
+});
+
+router.get("/rechercher", async (request, response) => {
+  const query = request.query;
+  const resultat = await promosController.rechercher(query);
+  response.status(200).json(resultat);
 });
 
 router.get("/:id", async (request, response) => {
@@ -14,29 +20,29 @@ router.get("/:id", async (request, response) => {
   response.status(200).json({ data });
 });
 
-router.post("/ajouter", wrapAsync(async (request, response, next) => {
-  const { titre, iteration } = request.body;
+router.post(
+  "/ajouter",
+  wrapAsync(async (request, response, next) => {
+    const { titre, iteration } = request.body;
 
-  validators.titre(titre, response);
-  validators.iteration(iteration, response);
+    validators.titre(titre, response);
+    validators.iteration(iteration, response);
 
-  const nouvellePromo = await promosController
-    .addPromo({ titre, iteration });
+    const nouvellePromo = await promosController.addPromo({ titre, iteration });
 
-  response.status(201).json({
-    data: {
-      titre: nouvellePromo.titre,
-      iteration: nouvellePromo.iteration
-    }
-  });
-}));
+    response.status(201).json({
+      data: {
+        titre: nouvellePromo.titre,
+        iteration: nouvellePromo.iteration
+      }
+    });
+  })
+);
 
 router.delete("/supprimer/:id", async (request, response) => {
   const { id } = request.params;
   await promosController.deletePromo(id);
-  response.json()
-    .status(204);
-
+  response.json().status(204);
 });
 router.put("/modifier/:id", async (request, response) => {
   const { id } = request.params;
@@ -49,8 +55,10 @@ router.post("/:id/apprenants/ajouter", async (request, response) => {
   const data = request.body;
   const promoId = request.params.id;
 
-  const nouvelApprenant = await promosController
-    .addApprenantDansPromo(promoId, data);
+  const nouvelApprenant = await promosController.addApprenantDansPromo(
+    promoId,
+    data
+  );
 
   response.status(201).json({
     data: {
@@ -58,14 +66,12 @@ router.post("/:id/apprenants/ajouter", async (request, response) => {
       prenom: nouvelApprenant.prenom
     }
   });
-})
+});
 
 module.exports = router;
 
-
-
 function wrapAsync(fn) {
-  return function (request, response, next) {
+  return function(request, response, next) {
     // Make sure to `.catch()` any errors and pass them along to the `next()`
     // middleware in the chain, in this case the error handler.
     fn(request, response, next).catch(next);
